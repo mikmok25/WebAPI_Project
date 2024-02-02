b2e69eaa9255dfc3963aebf8b9cd4ceea125ba92
Mick Nixon <103007309+mikmok25@users.noreply.github.com>
2024-02-02 12:06:08 -0500

    Added Comments on GreetingController.cs

--- WebAPI_Project/Controllers/GreetingController.cs
+++ WebAPI_Project/Controllers/GreetingController.cs
@@ -7,13 +7,25 @@ using System.Web.Http;
 
 namespace WebAPI_Project.Controllers
 {
+
+
     public class GreetingController : ApiController
     {
+        // <summary>
+        // This method returns a string "Hello World"
+        // usage:
+        //localhost:xx/greeting => "Hello World"
+        // </summary>
         public string Get()
         {
             return "Hello World";
         }
 
+        // <summary>
+        // This method returns a string "Hello to {number} people!"
+        // usage:
+        //localhost:xx/greeting/10 => "Hello to 10 people!"
+        // </summary>
         public string Get(string id)
         {
             return "Hello to " + id + " people!";
